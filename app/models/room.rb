7e700c24770a33028dfe3743d135c808a6bd9499
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-08 18:10:14 -0500

    [MOD] Pequeñas correcciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,7 +13,7 @@
 #
 
 class Room < ActiveRecord::Base
-  validates :description, length: { maximum: 400, message: "la descripción es muy larga" }
+  validates :description, length: { maximum: 400, too_long: "la descripción es muy larga" }
   validates :beds, numericality: { only_integer: true, message: "beds no es un número" }
   validates :guests, numericality: { only_integer: true, message: "guests no es un número" }
   validates :title, :description, :beds, :guests, :image_url, presence: true
